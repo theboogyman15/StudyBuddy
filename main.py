28f1e2f3ebc403238f75ebeb8162857dbfc96e52
theboogyman15 <thevishalrathod2005@gmail.com>
2026-03-15 15:00:50 +0530

    Initia; step to creatingg ui using gradio

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import ollama
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 import os
 import gradio as gr
 
-load_dotenv() 
+load_dotenv(override=True)
 model = os.getenv("MODEL_NAME")
-System = os.getenv("SYSTEM_PROMPT")
+system = os.getenv("SYSTEM_PROMPT")
+
+def chat(message, history):
+    formatted_history = [
+        {"role": h["role"], "content": h["content"] if isinstance(h["content"], str) else h["content"][0]["text"]}
+        for h in history
+    ]
+    all_messages = [{"role": "system", "content": system}] + formatted_history + [{"role": "user", "content": message}]
+    response = ollama.chat(model=model, messages=all_messages)
+    return response["message"]["content"]
+
+
+gr.ChatInterface(
+    fn=chat,
+    title="Ollama Chat",
+    description="Chat with Ollama"
+).launch()
